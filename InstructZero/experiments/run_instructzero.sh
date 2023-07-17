718669203f90f65a17cbe09907983eb0160c99d3
Lichang-Chen <Lichang-Chen@github.com>
2023-07-17 19:47:39 -0400

    update run.sh

--- InstructZero/experiments/run_instructzero.sh
+++ InstructZero/experiments/run_instructzero.sh
@@ -3,7 +3,9 @@ SFT=5
 RANDOM_PROJ='uniform'
 INTRINSIC_DIM=10
 # model_dir='lmsys/vicuna-13b-v1.3'
+# MODEL_NAME='vicuna'
 model_dir='WizardLM/WizardLM-13B-V1.1'
+MODEL_NAME='wizardlm' 
 
 datasets=(antonyms cause_and_effect common_concept diff)
 
@@ -15,5 +17,5 @@ for i in ${datasets[@]}; do
     --n_prompt_tokens $SFT \
     --intrinsic_dim $INTRINSIC_DIM \
     --HF_cache_dir ${model_dir} \
-    --model_name wizardlm
+    --model_name ${MODEL_NAME}
 done
